d6d48037709bc94b7bc92115d63d5f782b98a2c4
matikin9 <matikin9@gmail.com>
2014-12-16 17:36:45 -0800

    Various functions added.

--- js/cards.js
+++ js/cards.js
@@ -1,19 +1,23 @@
 var suits = [ "S", "H", "C", "D" ];
 var faces = [
-["A", [1, 11]],
-["2",[2]],
-["3",[3]],
-["4",[4]],
-["5",[5]],
-["6",[6]],
-["7",[7]],
-["8",[8]],
-["9",[9]],
-["T",[10]],
-["J",[10]],
-["Q",[10]],
-["K",[10]]
+    ["A", [1, 11]],
+    ["2",[2]],
+    ["3",[3]],
+    ["4",[4]],
+    ["5",[5]],
+    ["6",[6]],
+    ["7",[7]],
+    ["8",[8]],
+    ["9",[9]],
+    ["T",[10]],
+    ["J",[10]],
+    ["Q",[10]],
+    ["K",[10]]
 ];
+var deck = [];
+
+var DealerHand = [];
+var PlayerHand = [];
 
 function SortByNum(a, b) {
     var aNum = a.number;
@@ -31,19 +35,91 @@ function createCard(suit, face, num) {
 }
 
 function createDeck() {
-    var cards = [];
     for (var i = 0; i < suits.length; i++) {
         for (var j = 0; j < faces.length; j++) {
-            cards.push(createCard(suits[i], faces[j], Math.random()));
+            deck.push(createCard(suits[i], faces[j], Math.random()));
         }
     }
-    cards.sort(SortByNum);
-    return cards;
+    deck.sort(SortByNum);
 }
 
+function dealGame() {
+    DealerHand = [];
+    DealerHand.push(deck.shift());
 
+    PlayerHand = [];
+    PlayerHand.push(deck.shift());
+
+    debugShowNextTwoCards();
+
+    showDealerHand();
+    showPlayerHand();
+}
+
+function hitPlayer() {
+    PlayerHand.push(deck.shift());
+    debugShowNextTwoCards();
+
+    showDealerHand();
+    showPlayerHand();
+}
+
+function showDealerHand() {
+    var results = "";
+
+    for (var c in DealerHand) {
+        results += DealerHand[c].suit.toString() + DealerHand[c].face.toString() + " ";
+    }
+
+    $("#dealerHand").html("Dealer's hand: " + results);
+}
+
+function showPlayerHand() {
+    var results = "";
+
+    for (var c in PlayerHand) {
+        results += PlayerHand[c].suit.toString() + PlayerHand[c].face.toString() + " ";
+    }
+
+    $("#playerHand").html("Player's hand: " + results);
+}
+
+function debugShowNextTwoCards() {
+    $("#debug").html(deck[0].suit + deck[0].face + " " + deck[1].suit + deck[1].face);
+}
+
+function showDealerSum() {
+    
+}
+
+function showPlayerSum() {
+    
+}
 
 $().ready(function () {
-    var deck = createDeck();
-    $("#debug").html("first card: " + deck[0].suit + deck[0].face);
+    createDeck();
+    debugShowNextTwoCards();
+
+    $("button#deal").click(function () {
+        dealGame();
+    });
+
+    $("button#hit").click(function () {
+        hitPlayer();
+    });
+
+    $("button#stay").click(function () {
+
+    });
+
+    $("button#double").click(function () {
+
+    });
+
+    $("button#split").click(function () {
+
+    });
+
+    $("#dealerHand").html("Dealer's hand: " + DealerHand);
+    $("#playerHand").html("Player's hand: " + PlayerHand);
 });
